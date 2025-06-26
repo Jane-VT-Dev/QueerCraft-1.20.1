78f92f248b67e4eb7c08164d5af86e08a367b579
June-VT-Dev <actually.jane.mails@gmail.com>
2025-06-26 02:29:19 +0200

    WIP BonkStick

--- src/main/java/com/june/queercraft/item/BonkStick.java
+++ src/main/java/com/june/queercraft/item/BonkStick.java
@@ -2,11 +2,13 @@ package com.june.queercraft.item;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.RaycastContext;
 
 public class BonkStick extends Item {
@@ -14,15 +16,16 @@ public class BonkStick extends Item {
         super(settings);
     }
 
-
     @Override
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 
-        target.pushAwayFrom(attacker);
+ target.teleport();
+
         return super.postHit(stack, target, attacker);
+
+
+
     }
-
-
 }
 
 
